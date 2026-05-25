96cc4311500af8c0d36f687aaf37c421b1052f8d
Ishank <ishankmishra579@gmail.com>
2026-05-25 15:32:17 +0530

    drop the runner scratch file

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-ages = np.array([[21,17,19,20,16,30,18,65],
-                 [39,22,15,99,18,19,20,21]])
-teenagers = ages[ages<18]
-adults = ages[(ages>=18) & (ages<=65)]
-print(ages > 65)
