9d25d3e3812a6de3ad2ffa412ed0d4732c7aa97a
Ishank <ishankmishra579@gmail.com>
2026-05-25 15:32:17 +0530

    neaten the practice slice

--- practise.py
+++ practise.py
@@ -1,4 +1,10 @@
-a = [1,2,3,4,5,6,7]
-print(a[-3:][::-1])
+numbers = [1, 2, 3, 4, 5, 6, 7]
 
-print(a[-4::-1])
+
+def main():
+    print(f"last three reversed: {numbers[-3:][::-1]}")
+    print(f"from fourth-last backwards: {numbers[-4::-1]}")
+
+
+if __name__ == "__main__":
+    main()
